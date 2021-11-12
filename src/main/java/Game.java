19ae9c0e066f7c8910b8f7668d55b81726dc4e9c
Alexandre Correia <correiaalexandrealex@gmail.com>
2021-11-12 15:58:34 +0000

    Added keystroke input

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,20 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
     public Screen screen;
+    private int x = 10;
+    private int y = 10;
+
     public Game(){
         try {
             TerminalSize terminalSize = new TerminalSize(40, 20);
@@ -22,13 +28,34 @@ public class Game {
             e.printStackTrace();
         }
     }
+
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
     public void run() throws IOException {
-        draw();
+        KeyStroke key;
+        while (true){
+            draw();
+            key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
+                screen.close();
+            }
+            if (key.getKeyType() == KeyType.EOF){
+                break;
+            }
+            processKey(key);
+        }
+    }
+
+    private void processKey(KeyStroke key) throws IOException {
+        switch(key.getKeyType()){
+            case ArrowDown -> y += 1;
+            case ArrowUp -> y -= 1;
+            case ArrowLeft -> x -= 1;
+            case ArrowRight -> x += 1;
+        }
     }
 }
